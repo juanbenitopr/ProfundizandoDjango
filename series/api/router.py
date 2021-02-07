1c100d1390b706e9113a7fa97655000b00eb7089
Juan Benito <juanbenito.pacheco@smartprotection.com>
2021-02-07 20:01:11 +0100

    1.8 RETO: CRUD Capitulos y score de capítulos

--- series/api/router.py
+++ series/api/router.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import DefaultRouter
 
-from series.api.views import SeriesViewset
+from series.api.views import SeriesViewset, EpisodeViewset
 
 router = DefaultRouter()
 
 router.register(prefix='series', basename='series', viewset=SeriesViewset)
+router.register(prefix='episodes', basename='series', viewset=EpisodeViewset)
